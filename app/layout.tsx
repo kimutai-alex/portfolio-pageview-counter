1b28a57846df5d02bd168670c17fc8085f73dd05
kimutai-alex <72818216+kimutai-alex@users.noreply.github.com>
2024-03-01 22:36:55 +0300

    Update layout.tsx
    tried adding the url to point to jsxkit

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "jsxkit.com",
+    template: "%s | jsxkit.com",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
     title: "chronark.com",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
+    url: "https://jsxkit.com",
     siteName: "chronark.com",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://jsxkit.com/og.png",
         width: 1920,
         height: 1080,
       },
